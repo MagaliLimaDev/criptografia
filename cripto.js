cf9e3e829e36ddab9c879fae7b70eb0b40a166b8
MagaliLimaDev <mahrgfm@gmail.com>
2022-04-04 19:51:31 -0300

    Adicionei o input após selecionar a opção césar

--- cripto.js
+++ cripto.js
@@ -1,4 +1,4 @@
-document.querySelector('form').addEventListener('submit', function(event){
+document.querySelector("form").addEventListener("submit", function(event){
   event.preventDefault();
 })
 
@@ -14,16 +14,50 @@ function decodificarBase64(){
     document.getElementById("codificar").value = valorDecodificado;
 }
 
-var dicionario = () => {
+var alfabeto = () => {
   alfabeto.set(65, 'a' || 'A');
   alfabeto.set(66, 'b' || 'B');
   alfabeto.set(67, 'c' || 'C');
   alfabeto.set(68, 'd' || 'D');
   alfabeto.set(69, 'e' || 'E');
   alfabeto.set(70, 'f' || 'F');
+  alfabeto.set(71, 'g' || 'G');
+  alfabeto.set(72, 'h' || 'H');
+  alfabeto.set(73, 'i' || 'I');
+  alfabeto.set(74, 'j' || 'J');
+  alfabeto.set(75, 'k' || 'K');
+  alfabeto.set(76, 'l' || 'L');
+  alfabeto.set(77, 'm' || 'M');
+  alfabeto.set(78, 'n' || 'N');
+  alfabeto.set(79, 'o' || 'O');
+  alfabeto.set(80, 'p' || 'P');
+  alfabeto.set(81, 'q' || 'Q');
+  alfabeto.set(82, 'r' || 'R');
+  alfabeto.set(83, 's' || 'S');
+  alfabeto.set(84, 't' || 'T');
+  alfabeto.set(85, 'u' || 'U');
+  alfabeto.set(86, 'v' || 'V');
+  alfabeto.set(87, 'w' || 'W');
+  alfabeto.set(88, 'x' || 'X');
+  alfabeto.set(89, 'y' || 'Y');
+  alfabeto.set(90, 'z' || 'Z');
+  return alfabeto;
 }
-function codificarCesar(){
 
+function inserirCampo(){
+  var inputCesar = document.createElement("input");
+  inputCesar.type = "number";
+  document.getElementById("divCifra").appendChild(inputCesar);
+  
+}
+
+
+
+function codificarCesar(){
+  var cesar = document.getElementById("cifra");
+  for (var i = 0; i< cesar.length; i++);
+
+  
 
 }
 
