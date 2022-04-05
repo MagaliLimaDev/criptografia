cc7a80b2befdad1a4a9615782b263f41914ac526
MagaliLimaDev <mahrgfm@gmail.com>
2022-04-05 00:17:10 -0300

    adicionei o código da cifra de césar

--- cripto.js
+++ cripto.js
@@ -29,16 +29,37 @@ function inserirCampo(){
 }
 
 function codificarCesar(){
-  var deslocamento = document.getElementById("inputCesarNumber").value;
-  var texto = getElementById("codificar").value;
+  var deslocamento = parseInt(document.getElementById("inputCesarNumber").value);
+  var texto = document.getElementById("codificar").value;
   var textoMinusculo = texto.toLowerCase();
   var alfabeto = "abcdefghijklmnopqrstuvxwyz".split('');
   var novaString = '';
-}
 
-function decodificarCesar(){
+  for(i=0; i < textoMinusculo.length; i++){ //se cair no continue volta aqui
+    var letraAtual = textoMinusculo[i];
+    var numeroAtualAlfabeto = alfabeto.indexOf(letraAtual); //pega o valor numerico do alfabeto
+    var novoNumeroAlfabeto = (numeroAtualAlfabeto + deslocamento); //soma valor atual + o deslocamento
 
-}
+    if(letraAtual === ' '){
+      novaString = novaString + letraAtual; 
+      continue;    //necessário para não continuar executando as linhas abaixo, volta la no for
+    }
+    if(novoNumeroAlfabeto > 25){
+      novoNumeroAlfabeto = novoNumeroAlfabeto - 26;
+    }
+    if(novoNumeroAlfabeto < 0){
+      novoNumeroAlfabeto = novoNumeroAlfabeto + 26;
+    }
+    if(texto[i] === texto[i].toUpperCase()){ // compara se tem letras maiculas no texto
+      novaString = novaString + alfabeto[novoNumeroAlfabeto].toUpperCase(); //se tiver inclui como maiuscula msm
+    }else{
+      novaString = novaString + alfabeto[novoNumeroAlfabeto]; //inclui a nova string
+    }
+  }
+  document.getElementById("decodificar").value = novaString; //insere a novastring 
+
+};
+
  
 function verificarEncode(){
     var elemento = document.getElementsByName("cripto");
